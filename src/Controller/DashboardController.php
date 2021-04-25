3351be980d162a78ac8d9fd7a61c9f1479911c17
Al Zee <z@alz.ee>
2021-04-25 08:47:35 +0800

    dashboard/trans

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,14 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/dashboard")
+ */
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("/dashboard", name="dashboard")
+     * @Route("/", name="dashboard")
      */
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+
+    /**
+     * @Route("/trans", name="dashboard_trans")
+     */
+    public function trans(): Response
+    {
+        return $this->render('dashboard/trans.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
     }
