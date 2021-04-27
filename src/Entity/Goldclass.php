a96784f0d31d129667d194c53eea9eddf77f838f
Al Zee <z@alz.ee>
2021-04-27 19:39:38 +0800

    entity goldclass

---
+++ src/Entity/Goldclass.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GoldclassRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GoldclassRepository::class)
+ */
+class Goldclass
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
