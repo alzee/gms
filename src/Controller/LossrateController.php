490eccc1548333e26db56973e003cbe3cdae3bbc
Al Zee <z@alz.ee>
2021-04-27 20:18:38 +0800

    try form builder with form lossrate

--- src/Controller/LossrateController.php
+++ src/Controller/LossrateController.php
@@ -5,16 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Lossrate;
+use App\Form\LossrateType;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/lossrate")
+ */
 class LossrateController extends AbstractController
 {
     /**
-     * @Route("/lossrate", name="lossrate")
+     * @Route("/", name="lossrate")
      */
     public function index(): Response
     {
         return $this->render('lossrate/index.html.twig', [
             'controller_name' => 'LossrateController',
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="lossrate_new")
+     */
+    public function new(Request $request): Response
+    {
+        $l = new Lossrate();
+        $form = $this->createForm(LossrateType::class, $l);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $l = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($l);
+            $em->flush();
+
+            // return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render('lossrate/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
