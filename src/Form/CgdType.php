545f2249e2c70290773ba50c3330265ff11fad84
Al Zee <z@alz.ee>
2021-05-04 11:30:42 +0800

    placeholder

--- src/Form/CgdType.php
+++ src/Form/CgdType.php
@@ -3,17 +3,28 @@
 namespace App\Form;
 
 use App\Entity\Cgd;
+use App\Entity\Main;
+use App\Entity\Child;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class CgdType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            // ->add('main')
-            ->add('child')
+            ->add('main', EntityType::class , [
+                'placeholder' => '选择主单',
+                'class' => Main::class,
+                'required' => false
+            ])
+            ->add('child', EntityType::class , [
+                'placeholder' => '选择子单',
+                'class' => Child::class,
+                'required' => false
+            ])
             ->add('goldclass')
             ->add('position')
             ->add('weight')
