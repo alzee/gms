dd6b2ddabf23242a73da8f4cafce2c5af1dbf93c
Al Zee <z@alz.ee>
2021-05-10 13:57:15 +0800

    nth

--- public/js/z.js
+++ public/js/z.js
@@ -29,35 +29,61 @@ function showDoc() {
     let docTable = document.querySelector('#docTable');
     let saveBtn = document.querySelector('#saveBtn');
     fetch(location.origin + '/api/mains?sn=' + doc.value)
-    .then(response => response.json())
-    .then(data => {
-        if (data.length > 0) {
-            console.log(data);
-            let sn = document.querySelector('#sn');
-            let date = document.querySelector('#date');
-            let dueDate = document.querySelector('#dueDate');
-            let countPiece = document.querySelector('#countPiece');
-            let countChild = document.querySelector('#countChild');
-            let perWeight = document.querySelector('#perWeight');
-            let totalWeight = document.querySelector('#totalWeight');
-            let note = document.querySelector('#note');
-            sn.innerText = data[0].sn;
-            date.innerText = data[0].date;
-            dueDate.innerText = data[0].dueDate;
-            countPiece.innerText = data[0].countPiece;
-            countChild.innerText = data[0].countChild;
-            perWeight.innerText = data[0].perWeight;
-            totalWeight.innerText = data[0].totalWeight;
-            note.innerText = data[0].note;
-            docTable.classList.remove('d-none');
-            saveBtn.disabled = false;
-        }
-        else {
-            docTable.classList.add('d-none');
-            saveBtn.disabled = true;
-        }
-    }
-    );
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                console.log(data);
+                let sn = document.querySelector('#sn');
+                let date = document.querySelector('#date');
+                let dueDate = document.querySelector('#dueDate');
+                let countPiece = document.querySelector('#countPiece');
+                let countChild = document.querySelector('#countChild');
+                let perWeight = document.querySelector('#perWeight');
+                let totalWeight = document.querySelector('#totalWeight');
+                let note = document.querySelector('#note');
+                sn.innerText = data[0].sn;
+                date.innerText = data[0].date;
+                dueDate.innerText = data[0].dueDate;
+                countPiece.innerText = data[0].countPiece;
+                countChild.innerText = data[0].countChild;
+                perWeight.innerText = data[0].perWeight;
+                totalWeight.innerText = data[0].totalWeight;
+                note.innerText = data[0].note;
+                docTable.classList.remove('d-none');
+                saveBtn.disabled = false;
+            }
+            else {
+                fetch(location.origin + '/api/children?sn=' + doc.value)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.length > 0) {
+                            console.log(data);
+                            let sn = document.querySelector('#sn');
+                            let date = document.querySelector('#date');
+                            let dueDate = document.querySelector('#dueDate');
+                            let countPiece = document.querySelector('#countPiece');
+                            let countChild = document.querySelector('#countChild');
+                            let perWeight = document.querySelector('#perWeight');
+                            let totalWeight = document.querySelector('#totalWeight');
+                            let note = document.querySelector('#note');
+                            sn.innerText = data[0].sn;
+                            date.innerText = data[0].date;
+                            dueDate.innerText = data[0].dueDate;
+                            countPiece.innerText = data[0].countPiece;
+                            countChild.innerText = data[0].countChild;
+                            perWeight.innerText = data[0].perWeight;
+                            totalWeight.innerText = data[0].totalWeight;
+                            note.innerText = data[0].note;
+                            docTable.classList.remove('d-none');
+                            saveBtn.disabled = false;
+                        }
+                        else {
+                            docTable.classList.add('d-none');
+                            saveBtn.disabled = true;
+                        }
+                    })
+            }
+        });
 }
 
 function t() {
