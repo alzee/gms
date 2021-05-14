c842687a98e89211ecee6e2168737528fd13f371
Al Zee <z@alz.ee>
2021-05-14 17:00:11 +0800

    nth

--- src/Controller/CaController.php
+++ src/Controller/CaController.php
@@ -32,7 +32,7 @@ class CaController extends AbstractController
      */
     public function paginate(PaginatorInterface $paginator, Request $request): Response
     {
-        $dql = "select c from App\Entity\Ca c join c.child u order by c.id desc";
+        $dql = "select c, a from App\Entity\Ca c join c.child u join c.artisan a order by c.id desc";
         $query = $this->getDoctrine()->getManager()->createQuery($dql);
         $p = $paginator->paginate($query, $request->query->getInt('page', 1), 10);
 
