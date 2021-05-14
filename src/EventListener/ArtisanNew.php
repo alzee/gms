5d3daf51ea81e2d338ab77ad34bbfe212cc3f678
Al Zee <z@alz.ee>
2021-05-14 16:44:11 +0800

    nth

---
+++ src/EventListener/ArtisanNew.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author z14 <z@arcz.ee>
+ * @version
+ * @todo
+ */
+
+namespace App\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use App\Entity\Artisan;
+
+class ArtisanNew
+{
+
+    public function prePersist(Artisan $artisan, LifecycleEventArgs $event): void
+    {
+        $em = $event->getEntityManager();
+        $lastId = $em->getRepository(Artisan::class)->findOneBy([], ['id' => 'DESC'])->getId();
+        $artisan->setWn('A' . str_pad($lastId + 1, 4, '0', STR_PAD_LEFT));
+    }
+}
+
